e8444f72b391463c2e982134dd03cc03af16df40
djo1975 <mladanilic1975@gmail.com>
2023-10-09 09:42:08 +0200

    Apple and Orange

--- apple & orange.rb
+++ apple & orange.rb
@@ -19,3 +19,36 @@ def countApplesAndOranges(s, t, a, b, apples, oranges)
     puts apple_count
     puts oranges_count
 end
+
+# def countApplesAndOranges(s, t, a, b, apples, oranges)
+#     apple_count = 0
+#     oranges_count = 0
+    
+#     apple_positions = []
+#     orange_positions = []
+    
+#     for apple in apples
+#         position = a + apple
+#         apple_positions << position
+#     end
+    
+#     for orange in oranges
+#         position = b + orange
+#         orange_positions << position
+#     end
+    
+#     for position in apple_positions
+#         if position >= s && position <= t
+#             apple_count += 1
+#         end
+#     end
+    
+#     for position in orange_positions
+#         if position >= s && position <= t
+#             oranges_count += 1
+#         end
+#     end
+    
+#     puts apple_count
+#     puts oranges_count
+# end
