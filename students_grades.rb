99bdf70ce52388cffffc731b4cd574da21430fbb
djo1975 <mladanilic1975@gmail.com>
2023-10-10 10:31:58 +0200

    Grades

--- students_grades.rb
+++ students_grades.rb
@@ -20,3 +20,28 @@ def gradingStudents(grades)
 
     return rounded_grades
 end
+
+# def gradingStudents(grades)
+#     rounded_grades = []
+
+#     grades.each do |grade|
+#         if grade < 38
+#             # Ako je ocjena manja od 38, ne treba je zaokruživati
+#             rounded_grades << grade
+#         else
+#             # Provjeri ostatak pri dijeljenju ocjene s 5
+#             remainder = grade % 5
+
+#             if remainder >= 3
+#                 # Ako je ostatak veći ili jednak 3, zaokruži ocjenu prema sljedećem višekratniku broja 5
+#                 rounded_grades << grade + (5 - remainder)
+#             else
+#                 # Inače zadrži originalnu ocjenu jer nije potrebno zaokruživanje
+#                 rounded_grades << grade
+#             end
+#         end
+#     end
+
+#     # Vrati niz zaokruženih ocjena
+#     return rounded_grades
+# end
